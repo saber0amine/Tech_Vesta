f1845e21537f68be500244d9d983025abd36c865
saber0amine <120688951+saber0amine@users.noreply.github.com>
2023-12-30 18:46:37 +0100

    fix Http Security Request

--- blog/src/main/java/com/project/blog/config/SecurityConfig.java
+++ blog/src/main/java/com/project/blog/config/SecurityConfig.java
@@ -1,21 +1,31 @@
 package com.project.blog.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig   {
-	  
-	  @Bean
-	  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-	    http
-	        .authorizeHttpRequests(requests -> requests
-	            .requestMatchers(new AntPathRequestMatcher("/api/auth/**")).permitAll()
-	            .anyRequest().authenticated())
-	        .httpBasic();
-	    return http.build();
-	  }
+	
+	    @Bean
+	    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	        http.csrf().disable()
+	            .authorizeRequests(authorizeRequests ->
+	                authorizeRequests
+	                    .requestMatchers("/api/auth/**").permitAll()
+	                    .anyRequest().authenticated()
+	            );
+
+	        return http.build();
+	    }
+	    
+	    @Bean
+	     PasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder();
+	    }
 }
--- blog/src/main/java/com/project/blog/service/AuthService.java
+++ blog/src/main/java/com/project/blog/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.project.blog.service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.project.blog.dto.RegisterRequest;
@@ -12,13 +13,23 @@ public class AuthService {
 @Autowired
 private UserRepository userRepository ; 
 
+@Autowired
+private PasswordEncoder passwordEncoder ; 
+
+
+
 	   public void signup(RegisterRequest registerRequest) {
 	        User user = new User();
 	        user.setUserName(registerRequest.getUsername());
 	        user.setEmail(registerRequest.getEmail());
-	        user.setPassword(registerRequest.getPassword());
+	        user.setPassword(encodePassword(registerRequest.getPassword() )  );
 
 	        userRepository.save(user);
 	    } 
+	   
+	   
+	   private String encodePassword(String password) {
+		   return passwordEncoder.encode(password) ; 
+	   } 
 
 }
